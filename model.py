545465af15bbd90df0e49b2ea9b3e70ece84f100
Son The Phuong <25210032@ms.uit.edu.vn>
2026-09-19 14:10:08 +0700

    Step 7: decode_ids

--- model.py
+++ model.py
@@ -48,8 +48,11 @@ def decode_int(token_id, itos):
     # TODO: look up the character for token_id in the itos dict
     return itos.get(token_id, None)
 
-# Step 7 - decode_ids (not yet solved)
-# TODO: implement
+# Step 7 - decode_ids
+def decode_ids(ids, itos):
+    """Decode a list of token ids into a string using itos."""
+    # TODO: map each id through decode_int and join the characters into one string.
+    return "".join([decode_int(idx, itos) for idx in ids])
 
 # Step 8 - make_1d_array (not yet solved)
 # TODO: implement
