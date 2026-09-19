03296c33c7008d5b0ef61785a62dc963feea92ba
Son The Phuong <25210032@ms.uit.edu.vn>
2026-09-19 18:15:49 +0700

    Step 28: transpose_matrix

--- model.py
+++ model.py
@@ -215,8 +215,11 @@ def matmul(a, b):
         return None
     return a @ b
 
-# Step 28 - transpose_matrix (not yet solved)
-# TODO: implement
+# Step 28 - transpose_matrix
+def transpose_matrix(arr):
+    """Return the transpose of a 2D array."""
+    # TODO: return the transpose of arr using the .T attribute
+    return arr.T
 
 # Step 29 - sum_keepdims (not yet solved)
 # TODO: implement
