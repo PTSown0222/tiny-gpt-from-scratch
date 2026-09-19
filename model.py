5d51a931260a177c382d6ab692435d0dc0fa54af
Son The Phuong <25210032@ms.uit.edu.vn>
2026-09-19 18:26:10 +0700

    Step 31: softmax_overflow_demo

--- model.py
+++ model.py
@@ -240,8 +240,22 @@ def naive_softmax_1d(logits):
     result = numerator / denominator
     return result
 
-# Step 31 - softmax_overflow_demo (not yet solved)
-# TODO: implement
+# Step 31 - softmax_overflow_demo
+def softmax_overflow_demo(large_value):
+    """Show that naive exp overflows on a large logit.
+
+    Return {'naive_exp': float, 'overflowed': bool}.
+    """
+    # TODO: exponentiate large_value via array_exp and report whether it is inf.
+    arr = np.array([large_value])
+    results = dict()
+    numerator = array_exp(arr)
+    naive_exp = float(numerator[0])
+    overflowed = bool(np.isinf(naive_exp))
+    return {
+        'naive_exp': naive_exp,
+        'overflowed': overflowed
+    }
 
 # Step 32 - stable_softmax_1d (not yet solved)
 # TODO: implement
